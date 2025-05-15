d0fa7e849e1b4304151af66a4ebfb17063b8a0e4
jjHernandez17 <juanjohervar1708@gmail.com>
2025-05-15 18:44:46 -0500

    Add files via upload

--- hdv.py
+++ hdv.py
@@ -1,6 +1,13 @@
+from operator import truediv
+
+
 hoja_de_vida = {}
-diccionario = {}
+formaciones_aca = {}
 formaciones = []
+diccionario_experiencia = {}
+experiencias = []
+referencias = {}
+lista_habilidades = []
 print("---"*30)
 print("VITAECONSOLE")
 
@@ -25,7 +32,7 @@ if pregunt_formacion_academica == "si":
         años_academicos = input("Ingrese en que año terminó esta formacion:  \n")
         mas_formaciones = input("Tiene mas formaciones? (si/no) \n")
 
-        diccionario[formacion_academica] = {
+        formaciones_aca[formacion_academica] = {
             "formacion" : formacion_academica,
             "institucion" : institucion_academ,
             "titulo" : titulo,
@@ -36,33 +43,88 @@ if pregunt_formacion_academica == "si":
         if mas_formaciones == "si":
             print("")
         elif mas_formaciones == "no":
-            formaciones.append(diccionario)
+            formaciones.append(formaciones_aca)
+
             mas_formaciones_academic = False
 elif pregunt_formacion_academica == "no":
     print("no tiene formacion academica")
+
+
 
 
 ingresar_mas_experiencia = True
 while ingresar_mas_experiencia:
     exp_profesional  = input("Tiene experiencia en el ambito laboral? (si/no)\n")
     if exp_profesional == "si":
-        experiencia = input("Ingrese su experiencia: ")
-        empresas = input("Ingrese en que empresa adquirió la experiencia  \n")
+        experiencia = input("Ingrese su experiencia: \n ")
+        empresas = input("Ingrese en que empresa adquirió la experiencia:  \n")
         cargo = input("Ingrese el cargo que tenía en esta empresa \n ") 
         funcion = input("Ingrese la funcion que cumplia en esta empresa: \n")
+        duracion = input("Ingrese cuanto tiempo duró en la formacion: ")
+        diccionario_experiencia[experiencia] = {
+            "empresa" : empresas,
+            "cargo" : cargo,
+            "funcion" : funcion,
+            "duracion" : duracion,
+
+        }
+
         agregar_mas_exp = input("Desea agregar mas experiencia? ( si/no) \n")
         if agregar_mas_exp == "si":
             print("")
         elif agregar_mas_exp == "no":
+            experiencias.append(diccionario_experiencia)
             ingresar_mas_experiencia = False
     elif exp_profesional == "no":
         print("no")
         ingresar_mas_experiencia =False
     
-referencias = input("tiene referencias laborales o personales? (si/no) \n")
-if referencias == "si":
 
 
-    
+referencias_pregunta = input("tiene referencias laborales o personales? (si/no) \n")
+if referencias_pregunta == "si":
+    agg_referencia = True
+    while agg_referencia:
+        referencia = input("Ingrese el nombre de la empresa o persona que lo referencia: \n")
+        relacion_persona = input("Ingrese que relacion tiene con la persona: \n")
+        telefono_r = int(input("Ingrese el numero telefonico de la persona que lo referencia: \n"))
 
+        referencias[referencia] = {
+            "relacion" : relacion_persona,
+            "contacto": telefono_r
+        }
+
+        agregar_referencia = input("Desea agregar otra referencia? (si/no) \n")
+        if agregar_referencia == "si":
+            print("")
+        elif agregar_referencia == "no":
+            agg_referencia = False
+elif referencias_pregunta == "no":
+    print("No tiene referencias \n")
+
+
+
+agrega_mas_habilidades = True
+while agrega_mas_habilidades:
+    habilidad = input("Ingrese sus habilidades: \n")
+    mas_habilidades = input("Tiene mas habilidades: (si/no)")
+    lista_habilidades.append(habilidad)
+    if mas_habilidades == "si":
+        print("")
+    elif mas_habilidades == "no":
+        agrega_mas_habilidades = False
+
+hoja_de_vida[num_id] = {
+    "nombre" : nombre,
+    "contacto" : contacto,
+    "direccion": direccion,
+    "correo" : correo,
+    "fecha nacimiento": fecha_nacimiento,
+
+    "formaciones" : formaciones,
+    "experiencias" : experiencias,
+    "referencias" : referencias,
+    "habilidades"   : lista_habilidades
+
+}
 
